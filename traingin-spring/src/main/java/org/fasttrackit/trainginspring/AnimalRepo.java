472a00f91c8ce7f82bc913157e620c26f0e87378
MartonLak <72521563+MartonLak@users.noreply.github.com>
2021-01-05 04:49:03 +0200

    Adding

--- traingin-spring/src/main/java/org/fasttrackit/trainginspring/AnimalRepo.java
+++ traingin-spring/src/main/java/org/fasttrackit/trainginspring/AnimalRepo.java
@@ -3,6 +3,6 @@ package org.fasttrackit.trainginspring;
 import org.fasttrackit.trainginspring.Entity.Animals;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnimalRepo extends JpaRepository<Animals, Long>
+public interface  AnimalRepo extends JpaRepository<Animals, Long>
 {
 }
--- traingin-spring/src/main/java/org/fasttrackit/trainginspring/Controller/HelloController.java
+++ traingin-spring/src/main/java/org/fasttrackit/trainginspring/Controller/HelloController.java
@@ -29,17 +29,30 @@ public class HelloController
     }
 
     @PostMapping("/AddAnimals")
-    Animals newEmployee(@RequestBody Animals newAnimals)
+    Animals getNewAnimals(@RequestBody Animals newAnimals)
     {
         return repository.save(newAnimals);
     }
     @GetMapping("/FindAnimal/{id}")
-    Animals one(@PathVariable Long id) {
-
+    Animals FindAnimal(@PathVariable Long id) {
         return repository.findById(id).orElseThrow(() -> new AnymalException(id));
     }
+    @PutMapping("UpdateAnimals/{id}")
+    Animals replaceAnimals(@RequestBody Animals newAnimals, @PathVariable Long id)
+    {
+        return repository.findById(id).map(animals -> {
+            animals.setName(newAnimals.getName());
+            animals.setSpice(newAnimals.getSpice());
+            return repository.save(animals);
+        }).orElseGet(()->{
+            newAnimals.setId(id);
+            return repository.save(newAnimals);
+        });
+
+
+    }
     @DeleteMapping("/DeleteAnimals/{id}")
-    void deleteEmployee(@PathVariable Long id) {
+    void deleteAnimals(@PathVariable Long id) {
         repository.deleteById(id);
     }
 }
